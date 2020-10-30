5ee8f332bb9d444891d0d085d80b266ce71514d6
Egemen Kazanasmaz <egemenkazanasmaz@gmail.com>
2020-10-31 01:16:22 +0300

    Add time script

---
+++ Assets/RealiAI/Scripts/DataTypes/RealiAIPersonality.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RealiAI
+{
+    [System.Serializable, CreateAssetMenu(menuName = "RealiAI/Personality")]
+    public class RealiAIPersonality : ScriptableObject
+    {
+        [Range(0, 10)]
+        private int thirst;
+        [Range(0, 10)]
+        private int hunger;
+
+    }
+}
--- Assets/RealiAI/Scripts/RealiAICore.cs
+++ Assets/RealiAI/Scripts/RealiAICore.cs
@@ -38,36 +38,6 @@ namespace RealiAI
     }
 
     [System.Serializable]
-    public class Time
-    {
-        [SerializeField] private int hour = 00;
-        [SerializeField] private int minute = 00;
-        [SerializeField] private int second = 00;
-        private int seconds = 00;
-        
-        public Time(int hour, int minute, int second)
-        {
-            this.hour = hour;
-            this.minute = minute;
-            this.second = second;
-            this.seconds = (hour * 60 * 60) + (minute * 60) + second;
-        }
-
-        public Time(int seconds)
-        {
-            this.hour = Mathf.FloorToInt(seconds / (60 * 60));
-            this.minute = (hour * 60) -  Mathf.FloorToInt(minute / 60);
-            this.second = seconds - (hour * 60 * 60) - (minute * 60);
-            this.seconds = seconds;
-        }
-
-        public int Hour { get => hour; set => hour = value; }
-        public int Minute { get => minute; set => minute = value; }
-        public int Second { get => second; set => second = value; }
-        public int Seconds { get => seconds; set => seconds = value; }
-    }
-
-    [System.Serializable]
     public class Schedule
     {
     }
